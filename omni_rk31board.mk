b34bf263da0248d4c17ab16feeeaa27706c279fc
Matador-67 <Matador-67@users.noreply.github.com>
2015-10-12 08:21:38 +0200

    Update and rename aosp_rk31board.mk to omni_rk31board.mk

--- omni_rk31board.mk
+++ omni_rk31board.mk
@@ -6,8 +6,8 @@ PRODUCT_RELEASE_NAME := rk31board
 
 ## Device identifier. This must come after all inclusions
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := aosp_rk31board
+PRODUCT_NAME := omni_rk31board
 PRODUCT_DEVICE := rk31board
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on rk31board
+PRODUCT_MODEL := OMNI on rk31board
 PRODUCT_MANUFACTURER := Rockchip
